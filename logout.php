4df1410150ff5ca6d9bc3a642e84ba15fe83a836
JohnMoni91 <luisoujpof@gmail.com>
2024-10-05 10:42:37 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,7 @@
 <?php
-// logout.php
 session_start();
-session_unset(); // Remove todas as variáveis de sessão
-session_destroy(); // Destroi a sessão
+session_unset();
+session_destroy();
 header('Location: index.php');
 exit();
  
