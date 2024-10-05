6b24389405d6d7a07fd54fd22d940ba0ed9066bf
JohnMoni91 <luisoujpof@gmail.com>
2024-10-05 10:40:17 -0300

    add_comment.php

--- add_comment.php
+++ add_comment.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $post_id = $_POST['post_id'];
     $comment_text = $_POST['comment_text'];
 
-    // Verifique se o post_id existe na tabela postagens
+ 
     $sql_check_post = "SELECT COUNT(*) FROM postagens WHERE id = ?";
     $stmt_check_post = $conn->prepare($sql_check_post);
     $stmt_check_post->bind_param("i", $post_id);
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
 
-    // Inserindo o comentário
+
     $sql_insert = "INSERT INTO comments (post_id, comment_text, comment_date) VALUES (?, ?, NOW())";
     $stmt_insert = $conn->prepare($sql_insert);
     $stmt_insert->bind_param("is", $post_id, $comment_text);
