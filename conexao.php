6c767c1ff25f4939f1e4e08c540cc249fd843e6f
JohnMoni91 <luisoujpof@gmail.com>
2024-10-05 10:41:07 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -5,10 +5,8 @@ $username = "root";
 $password = "";
 $dbname = "diario";
 
-// Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica a conexão
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
