df5bab95471480817b102daa7b2ab0f91b506331
Joao Nacur <luisoujpof@gmail.com>
2024-10-08 11:49:36 -0300

    Add files via upload

--- diario.sql
+++ diario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07/10/2024 às 04:18
+-- Tempo de geração: 08/10/2024 às 16:17
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
